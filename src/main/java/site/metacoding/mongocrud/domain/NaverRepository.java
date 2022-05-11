b34fd6a06219150eb6cc3dd77c349ddf7d7b5ac9
woals990605 <woals990605@naver.com>
2022-05-11 12:39:35 +0900

    몽고DB + 통합테스트 + 네이티브쿼리

---
+++ src/main/java/site/metacoding/mongocrud/domain/NaverRepository.java
@@ -0,0 +1,7 @@
+package site.metacoding.mongocrud.domain;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NaverRepository extends MongoRepository<Naver, String> {
+
+}
